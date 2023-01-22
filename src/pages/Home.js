f9eb8b695d7d31c303d97769224d559cd31253b8
sara.sadeghnezhad <sarasadeghnejad@yahoo.com>
2023-01-22 22:00:26 +0330

    ad neww components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,16 @@
-import { Stack } from '@mui/material'
+import { Box } from '@mui/material';
 import React from 'react'
-import Logo from '../assets/images/Logo.png';
+import Exercises from '../components/Exercises';
+import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
 
 const Home = () => {
   return (
-    <Stack direction="row" justifyContent="space-around">
-    <Link to='/'>
-        <img src={Logo} alt="logo" style={{width:"48px",height:"48px",margin:"0 20px"}}/>
-        </Link>
-        <Stack direction='row' fontSize="24px" gap='40px' alignItems="flex-end">
-            <Link to='/' style={{textDecoration:"none",color:"#3A1212",borderBottom:"3px solid #FF2625"}}>Home</Link>
-            <a href="/" style={{textDecoration:"none",color:"#3A1212"}}>Exercises</a>
-        </Stack>
-    </Stack>
+   <Box>
+    <HeroBanner />
+    <SearchExercises />
+    <Exercises /> 
+   </Box>
   )
 }
 
